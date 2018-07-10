2dde882c5fbb203fb21725738c56b4ce8c29d66e
lauratan878 <lauratan878@gmail.com>
2018-07-10 20:28:05 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,25 +9,79 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
-  
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room[0]
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room[4]
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room[6]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room.first
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+puts room[2]
+puts room[3]
 end
